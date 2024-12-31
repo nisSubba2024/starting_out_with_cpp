a65e46a5201e42e092a74f9e7ffd7eb31e05c978
Nishan Subba <nishansubba@macbookpro.mynetworksettings.com>
2024-12-30 19:22:21 -0500

    continue

--- Programming_Challenges/chapter_2/main.cpp
+++ Programming_Challenges/chapter_2/main.cpp
@@ -82,5 +82,85 @@ int main()
     cout << "In 10 years, the ocean's level will be risen by " << oceanLevelsIn10Years << " millimeter" << endl;
 
 
+    // Question 8
+    cout << "\n***** Question 8: Total Purchase *****" << endl;
+    double item1 = 15.95;
+    double item2 = 24.95;
+    double item3 = 6.95;
+    double item4 = 12.95;
+    double item5 = 3.95;
+    const double SALETAX = 0.07;
+    double preTaxTotal = item1 + item2 + item3 + item4 + item5;
+    double totalTaxQ8 = preTaxTotal * SALETAX;
+    double totalAmount = preTaxTotal + totalTaxQ8;
+    cout << "Total subtotal: $" << preTaxTotal << "\nTotal Tax: $" << totalTaxQ8 << "\nTotal Amount: $" << totalAmount << endl;
+
+
+    // Question 9
+    cout << "\n***** Question 9: Cyborg Data Type Sizes *****" << endl;
+    cout << "Char as " << sizeof(char) << " bytes" << endl;
+    cout << "Int as " << sizeof(int) << " bytes" << endl;
+    cout << "Float as " << sizeof(float) << " bytes" << endl;
+    cout << "Double as " << sizeof(double) << " bytes" << endl;
+
+    // Question 10
+    cout << "\n***** Question 10: Miles per Gallon *****" << endl;
+    int gallons = 15;
+    int distanceLimit = 375;
+    // Miles per gallon = miles driven / gallons usd
+    double milesPerGallon = double(distanceLimit) / gallons;
+    cout << "If car can hold " << gallons << " gallons and can travel " << distanceLimit << " miles before refueling, then its miles per gallon is " << milesPerGallon << endl;
+
+    // Question 11
+    cout << "\n***** Question 11: Distance per Tank of Gas *****" << endl;
+    int carTank = 20;
+    double avgCityMPG = 23.5;
+    double avgHighwayMPG = 28.9;
+    // distance = number of gallon x avg miles per gallon
+    double cityDistance = avgCityMPG * carTank;
+    double highwayDistance = avgHighwayMPG * carTank;
+    cout << "Car with " << carTank << " gallons with: " << endl;
+    cout << "Average city " << avgCityMPG << " miles per gallon will travel total of " << cityDistance << " miles" << endl;
+    cout << "Average highway " << avgHighwayMPG << " miles per gallon will travel total of " << highwayDistance << " miles" << endl;
+
+    // Question 12
+    cout << "\n***** Question 12: Land Calculation *****" << endl;
+    // one acres = 43,560 sq feet
+    double oncAcre = 43560; // sq ft
+    int totalLand = 391876;
+    double totalAcre = totalLand / oncAcre;
+    cout << "There are " << totalAcre << " acres in land with " << totalLand << " sq feet";
+
+    // Question 13
+    cout << "\n***** Question 13: Circuit Board Price *****" << endl;
+
+
+    // Question 14
+    cout << "\n***** Question 14: Personal Information *****" << endl;
+
+
+    // Question 15
+    cout << "\n***** Question 15: Triangle Pattern *****" << endl;
+
+
+    // Question 16
+    cout << "\n***** Question 16: Diamond Pattern *****" << endl;
+
+
+    // Question 17
+    cout << "\n***** Question 17: Stock Commission *****" << endl;
+
+
+    // Question 18
+    cout << "\n***** Question 18: Energy Drink Consumption *****" << endl;
+
+
+    // Question 19
+    cout << "\n***** Question 19: Annual High Temperatures *****" << endl;
+
+
+    // Question 20
+    cout << "\n***** Question 20: How Much Paint *****" << endl;
+
     return 0;
 }
